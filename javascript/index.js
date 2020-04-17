3d7f6ca58b5c0f0a89d8ba0f27d9c417a13eee59
A-312 <A-312@users.noreply.github.com>
2020-04-17 16:34:24 +0000

    Deploying to gh-pages from  - c045d318e9b8018bee8a8221e2dc2d2ed528383e 🚀

---
+++ javascript/index.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){const t=document.createElement("script");t.setAttribute("async",!0),t.setAttribute("src","javascript/game.js?t="+ +new Date),document.head.appendChild(t)}));
+//# sourceMappingURL=../../build/javascript/index.js.map
