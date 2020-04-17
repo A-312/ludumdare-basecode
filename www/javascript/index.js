86122db0efcc595f0215b06d0978d7f8aa00ac39
A-312 <A-312@users.noreply.github.com>
2020-04-17 18:34:37 +0200

    fix-eslint

--- www/javascript/index.js
+++ www/javascript/index.js
@@ -1,7 +1,7 @@
 // Try to prevent bug
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
   const script = document.createElement('script')
-  script.setAttribute('async',true)
-  script.setAttribute('src','javascript/game.js?t=' + (+new Date()))
+  script.setAttribute('async', true)
+  script.setAttribute('src', 'javascript/game.js?t=' + (+new Date()))
   document.head.appendChild(script)
 })
